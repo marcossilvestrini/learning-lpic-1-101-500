f26e8098fd0ffe6e1c01c6ae01eca03d7da86df4
marcos <marcos.silvestrini@gmail.com>
2021-11-04 17:12:33 -0300

    fix: Fix file

--- Scripts/vagrant_up_windows.ps1
+++ Scripts/vagrant_up_windows.ps1
@@ -8,11 +8,9 @@ vagrant up
 
 #up centos
 Set-Location $centos
-
 #download box if error certificate
 #vagrant box add --insecure centos/8
 vagrant up
-
 
 #Fix powershell error
 $Env:VAGRANT_PREFER_SYSTEM_BIN += 0
